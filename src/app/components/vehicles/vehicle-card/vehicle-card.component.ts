c55602731a138a58b69fb7c6842d7cc172ae5034
konrad <webmastering1997@gmail.com>
2026-02-14 16:51:16 +0100

    MotoPassionClient - vehicleEdit - Layout change in edit vehicle card.: #3

--- src/app/components/vehicles/vehicle-card/vehicle-card.component.ts
+++ src/app/components/vehicles/vehicle-card/vehicle-card.component.ts
@@ -2,6 +2,8 @@ import {Component, Input} from '@angular/core';
 import {Vehicle} from '../../../model/vehicle.model';
 import {CommonModule} from '@angular/common';
 import {VEHICLE_PETROL} from '../vehicle-utils/vehicle-utils';
+import {VehicleService} from '../../../services/vehicle/vehicle.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-vehicle-card',
@@ -13,11 +15,23 @@ import {VEHICLE_PETROL} from '../vehicle-utils/vehicle-utils';
 export class VehicleCardComponent {
   @Input() vehicle!: Vehicle;
    vehiclePetrol = VEHICLE_PETROL;
-
+constructor(private router: Router, private vehicleService: VehicleService) {
+}
   getfuelLabel(): string {
     if(this.vehicle) {
       return <string>this.vehiclePetrol.at(this.vehicle.fuelType);
     }
     return '-';
+  }
+  edit() {
+    this.router.navigate(['/vehicles/edit', this.vehicle.id]);
+  }
+
+  remove() {
+    if (!confirm('Usunąć pojazd?')) return;
+
+    this.vehicleService.delete(this.vehicle.id).subscribe({
+      next: () => window.location.reload()
+    });
   }
 }
